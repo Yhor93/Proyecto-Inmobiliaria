1d0ad8f4a68d5d499efa4ce8666f9e1de5a76954
Yorman Pabon <yormanqp@gmail.com>
2022-10-28 17:24:35 -0500

    LoopBAck Completo

---
+++ LoopBack/BackendInmobiliaria/src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './ping.controller';
+export * from './cliente-solicitud.controller';
+export * from './asesor-solicitud.controller';
+export * from './asesor-inmueble.controller';
+export * from './administrador-asesor.controller';
+export * from './solicitud-inmueble.controller';
