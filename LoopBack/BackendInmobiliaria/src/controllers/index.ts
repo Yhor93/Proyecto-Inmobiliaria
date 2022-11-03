5b297faf95e2dcf8a2a3f3ad71c3ebc9c5eb12bb
Yorman Pabon <yormanqp@gmail.com>
2022-11-02 22:48:24 -0500

    Modificacion de los Controllers

--- LoopBack/BackendInmobiliaria/src/controllers/index.ts
+++ LoopBack/BackendInmobiliaria/src/controllers/index.ts
@@ -4,3 +4,8 @@ export * from './asesor-solicitud.controller';
 export * from './asesor-inmueble.controller';
 export * from './administrador-asesor.controller';
 export * from './solicitud-inmueble.controller';
+export * from './cliente.controller';
+export * from './administrador.controller';
+export * from './asesor.controller';
+export * from './solicitud.controller';
+export * from './inmueble.controller';
