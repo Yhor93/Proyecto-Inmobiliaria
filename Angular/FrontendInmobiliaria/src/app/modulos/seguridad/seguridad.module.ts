67114b892130a8f1b87b8ac706dad99f5f25bed5
Yorman Pabon <yormanqp@gmail.com>
2022-11-14 20:52:28 -0500

    Validacion-Login

--- Angular/FrontendInmobiliaria/src/app/modulos/seguridad/seguridad.module.ts
+++ Angular/FrontendInmobiliaria/src/app/modulos/seguridad/seguridad.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
 import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     LoginComponent,
     CambioClaveComponent,
     RecuperarClaveComponent,
+    LogoutComponent,
     
   ],
   imports: [
