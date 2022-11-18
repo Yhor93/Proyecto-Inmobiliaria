d1ad2dd2234bf4f0a4ed56c07fdc648e92d0c346
Yorman Pabon <yormanqp@gmail.com>
2022-11-18 16:29:11 -0500

    formulario Crear Usuario

--- Angular/FrontendInmobiliaria/src/app/modulos/administracion/usuarios/crear-usuario/crear-usuario.component.ts
+++ Angular/FrontendInmobiliaria/src/app/modulos/administracion/usuarios/crear-usuario/crear-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DatosUserModel } from 'src/app/modelos/datos-user.model';
+import { RolesModel } from 'src/app/modelos/roles.model';
+import { SeguridadService } from 'src/app/servicios/shared/seguridad.service';
 
 @Component({
   selector: 'app-crear-usuario',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearUsuarioComponent implements OnInit {
 
-  constructor() { }
+  listaRoles:RolesModel[]=[];
+  formularioRegistro: FormGroup = new FormGroup({});
 
-  ngOnInit(): void {
+  constructor(
+    private fb:FormBuilder,
+    private servicioSeguridad: SeguridadService
+  ){ }
+   
+  ngOnInit(): void {  
+    this.roles();
   }
 
+  formulario(){
+    this.formularioRegistro=this.fb.group({
+      nombre:[""],
+      cedula:[""],
+      celular:[""],
+      email:[""],
+      ciudad:[""],
+      roles:[""]
+    })
+  }
+
+  registroUsuarios(){
+    if (this.formularioRegistro.invalid) {
+      alert("Campos Obligatorios");
+    } else {
+      let usuario = new DatosUserModel();
+      usuario.nombre=this.formularioRegistro.controls['nombre'].value;
+      usuario.cedula=this.formularioRegistro.controls['cedula'].value;
+      usuario.celular=this.formularioRegistro.controls['celular'].value;
+      usuario.email=this.formularioRegistro.controls['email'].value;
+      usuario.ciudad=this.formularioRegistro.controls['ciudad'].value;
+      usuario.roles=this.formularioRegistro.controls['roles'].value;
+    }
+  }
+
+  roles(){
+    this.servicioSeguridad.obtenerRoles().subscribe({
+      next: (data:RolesModel[])=>{
+        this.listaRoles=data;
+      },
+        error:(e)=> console.log(e)
+    })
+  }
 }
