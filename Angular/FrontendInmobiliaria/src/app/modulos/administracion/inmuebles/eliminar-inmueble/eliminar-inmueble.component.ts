f9d46358105c321830b3f187a72680de0b60931c
“Ramiro” <ramiro.cruzado@ingenieros.com>
2022-11-20 22:56:50 -0500

    Crud inmuebles

--- Angular/FrontendInmobiliaria/src/app/modulos/administracion/inmuebles/eliminar-inmueble/eliminar-inmueble.component.ts
+++ Angular/FrontendInmobiliaria/src/app/modulos/administracion/inmuebles/eliminar-inmueble/eliminar-inmueble.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { inmuebleModel } from 'src/app/modelos/inmueble.model';
+import { BuscarInmueblesService } from 'src/app/servicios/inmuebles/buscar-inmuebles.service';
+declare const generarVentanaModal:any;
 
 @Component({
   selector: 'app-eliminar-inmueble',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EliminarInmuebleComponent implements OnInit {
 
-  constructor() { }
+  id:string= this.route.snapshot.params["id"];
+  inmueble: inmuebleModel = new inmuebleModel();
+
+  constructor(
+    private servicioInmueble: BuscarInmueblesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.servicioInmueble.consultarinmueble(this.id).subscribe({
+      next:(data: inmuebleModel)=>{
+        this.inmueble=data;
+      }
+    })
+  }
+
+  Eliminar(){
+    this.servicioInmueble.EliminarInmueble(this.id).subscribe({
+      next:(data:any)=>{
+        generarVentanaModal("Registro Eliminado Exitosamente");
+        this.router.navigate(["administracion/buscar-inmueble"]);
+      },
+      error: (e)=>console.log(e)
+      
+    })
   }
 
 }
